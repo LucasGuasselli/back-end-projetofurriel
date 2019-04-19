a48006ccacde88e1a7909026cf2a8b78828d6b60
Lucas Guasselli <Lucaszack265@gmail.com>
2019-04-19 14:22:45 -0300

    Projeto criado

---
+++ src/main/java/com/lucasguasselli/projetofurriel/ProjetofurrielApplication.java
@@ -0,0 +1,13 @@
+package com.lucasguasselli.projetofurriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofurrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofurrielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasguasselli/projetofurriel/ProjetofurrielApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucasguasselli.projetofurriel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetofurrielApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
