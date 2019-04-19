f0731820e336b887756aed4d103363d099eee332
Lucas Guasselli <Lucaszack265@gmail.com>
2019-04-19 16:53:15 -0300

    Testando a primeira classe de dominio - PostoGraduacao

---
+++ src/main/java/com/lucasguasselli/projetofurriel/domain/PostoGraduacao.java
@@ -0,0 +1,85 @@
+package com.lucasguasselli.projetofurriel.domain;
+
+import java.io.Serializable;
+
+
+public class PostoGraduacao implements Serializable{
+
+	// esta versao a baixo se faz necessaria por causa do Serializable (padrao java)
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+	private double soldo;
+	private double cotaParte;
+	
+	public PostoGraduacao() {
+		
+	}
+
+	public PostoGraduacao(int id, String nome, double soldo, double cotaParte) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.soldo = soldo;
+		this.cotaParte = cotaParte;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSoldo() {
+		return soldo;
+	}
+
+	public void setSoldo(double soldo) {
+		this.soldo = soldo;
+	}
+
+	public double getCotaParte() {
+		return cotaParte;
+	}
+
+	public void setCotaParte(double cotaParte) {
+		this.cotaParte = cotaParte;
+	}
+
+	// HashCode e Equals para comparacoes de Objetos
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PostoGraduacao other = (PostoGraduacao) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}	
+	
+	
+}
--- src/main/java/com/lucasguasselli/projetofurriel/resources/PostoGraduacaoResource.java
+++ src/main/java/com/lucasguasselli/projetofurriel/resources/PostoGraduacaoResource.java
@@ -1,15 +1,27 @@
 package com.lucasguasselli.projetofurriel.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasguasselli.projetofurriel.domain.PostoGraduacao;
 
 @RestController
 @RequestMapping(value="/postosGraduacoes")
 public class PostoGraduacaoResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<PostoGraduacao> listar() {
+		PostoGraduacao soldadoEV = new PostoGraduacao(1, "Soldado EV", 854.00, 37.576 );
+		PostoGraduacao soldadoEP = new PostoGraduacao(2, "Soldado EP", 1758.00, 65.032);
+		
+		List<PostoGraduacao> lista = new ArrayList<>();
+			lista.add(soldadoEV);
+			lista.add(soldadoEP);
+			
+				return lista;
 	}
 }
