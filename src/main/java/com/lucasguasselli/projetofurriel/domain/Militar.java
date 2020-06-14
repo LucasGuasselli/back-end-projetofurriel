310559c585510be36fc3f377c4aaf0078a1c884f
Lucas Guasselli <Lucaszack265@gmail.com>
2020-06-14 09:00:41 -0300

    Alterando métodos do front-end para Pt-Br

--- src/main/java/com/lucasguasselli/projetofurriel/domain/Militar.java
+++ src/main/java/com/lucasguasselli/projetofurriel/domain/Militar.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -15,7 +17,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Militar {
-
+			
 	@Id
 	private int precCP;
 	private String nome;
